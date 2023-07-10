de5103d6f03790501763c4c2792f407ccfab880f
Md Sahil Alam <mdsahilalam11@gmail.com>
2023-07-10 14:36:51 +0530

    404 page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Suspense } from "react";
 // import "./assets/css/styles.css";
-import "./App.css"
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import routes from "./routes";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
@@ -11,13 +12,16 @@ export default function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <AuthProvider>
           <Routes>
-            {Object.keys(routes).map((key) => (
-              <Route
-                key={key}
-                path={routes[key].path}
-                element={routes[key].element}
-              />
-            ))}
+            <>
+              {Object.keys(routes).map((key) => (
+                <Route
+                  key={key}
+                  path={routes[key].path}
+                  element={routes[key].element}
+                />
+              ))}
+              <Route path='*' element={<NotFound />}/>
+            </>
           </Routes>
         </AuthProvider>
       </Suspense>
